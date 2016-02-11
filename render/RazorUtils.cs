88706ed538430908f17dfff452a580c2cff33269
leedavi <dcl@nevoweb.com>
2016-02-11 18:10:15 +0100

    renderpaging fucntion + ck config

--- render/RazorUtils.cs
+++ render/RazorUtils.cs
@@ -46,6 +46,83 @@ namespace NBrightDNN.render
             return result;
         }
 
+        public static String RenderPaging(int totalRecords, int currentPage = 1, int pageSize = 20, String wrapperClass = "NBrightPagingDiv", String normalclass = "NBrightNormalPg", String selectclass = "NBrightSelectPg", String nextclass = "NBrightNextPg", String previousclass = "NBrightPrevPg", String linkclass = "cmdPg",String cssNextSection = "NBrightNextSection",String cssNextPage = "NBrightNextPg",String cssLastPage = "NBrightLastPg",String cssPrevSection = "NBrightPrevSection",String cssPrevPage = "NBrightPrevPg",String cssFirstPage = "NBrightFirstPg")
+        {
+            var strOut = "";
+
+            var lastPage = Convert.ToInt32(totalRecords / pageSize);
+            if (totalRecords != (lastPage * pageSize)) lastPage = lastPage + 1;
+            if (lastPage == 1) return ""; //if only one page, don;t process
+
+            if (currentPage <= 0) currentPage = 1;
+            const int pageLinksPerPage = 10;
+            var rangebase = Convert.ToInt32((currentPage - 1) / pageLinksPerPage);
+            var lowNum = (rangebase * pageLinksPerPage) + 1;
+            var highNum = lowNum + (pageLinksPerPage - 1);
+            if (highNum > Convert.ToInt32(lastPage)) highNum = Convert.ToInt32(lastPage);
+            if (lowNum < 1) lowNum = 1;
+
+            strOut += "<div class='" + wrapperClass + "'><ul>";
+
+
+            #region "first section"
+
+            if ((lowNum != 1) && (currentPage > 1))
+            {
+                strOut += "<li class='" + cssFirstPage + "'><a pagenumber='1' class='" + linkclass + "'>&lt;&lt;</a></li>";
+            }
+
+            if (currentPage > 1)
+            {
+                strOut += "<li class='" + cssPrevPage + "'><a pagenumber='" + (currentPage - 1) + "' class='" + linkclass + "'>&lt;</a></li>";
+            }
+
+            if (lowNum > 1)
+            {
+                strOut += "<li class='" + cssPrevSection + "'><a pagenumber='" + (lowNum - 1) + "' class='" + linkclass + "'>...</a></li>";
+            }
+
+            #endregion
+
+            // body section
+            for (int i = lowNum; i <= highNum; i++)
+            {
+                if (currentPage == i)
+                {
+                    strOut += "<li class='" + selectclass + "'><a pagenumber='" + i + "' class='" + linkclass + "'>" + i + "</a></li>";
+                }
+                else
+                {
+                    strOut += "<li class='" + normalclass + "'><a pagenumber='" + i + "' class='" + linkclass + "'>" + i + "</a></li>";
+                }
+            }
+
+            #region "last section"
+
+            if ((lastPage > highNum))
+            {
+                strOut += "<li class='" + cssNextSection + "'><a pagenumber='" + (highNum + 1) + "' class='" + linkclass + "'>...</a></li>";
+            }
+            if ((lastPage > currentPage))
+            {
+                strOut += "<li class='" + cssNextPage + "'><a pagenumber='" + (currentPage + 1) + "' class='" + linkclass + "'>&gt;</a></li>";
+            }
+
+            if ((lastPage != highNum) && (lastPage > currentPage))
+            {
+                strOut += "<li class='" + cssLastPage + "'><a pagenumber='" + lastPage + "' class='" + linkclass + "'>&gt;&gt;</a></li>";
+            }
+
+            #endregion
+
+
+
+            strOut += "</ul></div>";
+
+            return strOut;
+        }
+
+
     }
 
 }
